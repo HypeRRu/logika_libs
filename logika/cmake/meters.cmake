d31799357fa559d354529b4189fb58fba220aa88
HypeRRu <hyperru1lvl@gmail.com>
2024-04-06 16:36:43 +0300

    logika: some windows build fixes, WIP

--- logika/cmake/meters.cmake
+++ logika/cmake/meters.cmake
@@ -34,7 +34,7 @@ set(
 )
 
 add_library(${LIB_NAME} ${${LIB_NAME}_SOURCES_LIST})
-target_link_libraries(${LIB_NAME} PUBLIC ${CMAKE_PROJECT_NAME}_common)
 target_include_directories(${LIB_NAME} PUBLIC ${INCLUDE_DIR})
+target_link_libraries(${LIB_NAME} INTERFACE ${CMAKE_PROJECT_NAME}_common)
 
 unset(${LIB_NAME})
--- logika/cmake/resource_converters.cmake
+++ logika/cmake/resource_converters.cmake
@@ -14,11 +14,11 @@ set(
 )
 
 add_library(${LIB_NAME} ${${LIB_NAME}_SOURCES_LIST})
-target_link_libraries(${LIB_NAME} PUBLIC ${CMAKE_PROJECT_NAME}_resources)
-target_link_libraries(${LIB_NAME} PUBLIC ${CMAKE_PROJECT_NAME}_logger)
-target_link_libraries(${LIB_NAME} PUBLIC ${CMAKE_PROJECT_NAME}_common)
-target_link_libraries(${LIB_NAME} PUBLIC ${CMAKE_PROJECT_NAME}_meters)
 target_include_directories(${LIB_NAME} PUBLIC ${INCLUDE_DIR})
 target_include_directories(${LIB_NAME} PUBLIC ${logika_resources_INCLUDE_DIR})
+target_link_libraries(${LIB_NAME} INTERFACE ${CMAKE_PROJECT_NAME}_resources)
+target_link_libraries(${LIB_NAME} INTERFACE ${CMAKE_PROJECT_NAME}_logger)
+target_link_libraries(${LIB_NAME} INTERFACE ${CMAKE_PROJECT_NAME}_common)
+target_link_libraries(${LIB_NAME} INTERFACE ${CMAKE_PROJECT_NAME}_meters)
 
 unset(${LIB_NAME})
