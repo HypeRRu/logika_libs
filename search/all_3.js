716b2662ba64d4b49c64cac83e0d0a892234f3b8
HypeRRu <HypeRRu@users.noreply.github.com>
2024-05-03 14:22:07 +0000

    deploy: e232f30dba5b06f91544d316a7ae9a1addb84ed6

--- search/all_3.js
+++ search/all_3.js
@@ -64,8 +64,8 @@ var searchData=
   ['dohandshake_61',['DoHandshake',['../d2/d4d/classlogika_1_1protocols_1_1M4_1_1M4Protocol.html#a13e626133daa4525e4e2144c32f87c51',1,'logika::protocols::M4::M4Protocol']]],
   ['dolegacyrequest_62',['DoLegacyRequest',['../d2/d4d/classlogika_1_1protocols_1_1M4_1_1M4Protocol.html#a82ee9b0a8dfd6574f531adc51890ada7',1,'logika::protocols::M4::M4Protocol']]],
   ['dom4request_63',['DoM4Request',['../d2/d4d/classlogika_1_1protocols_1_1M4_1_1M4Protocol.html#a88ce4a93c3212b102a5db6bfc8e837a6',1,'logika::protocols::M4::M4Protocol']]],
-  ['dontgeteus_64',['DontGetEus',['../d5/d7f/namespacelogika_1_1protocols_1_1M4_1_1TagsUpdateFlags.html#a9cc38c58b6e1c5dd0d9b0d06c893b7b2a923f3ef96639372f096724f1835568c7',1,'logika::protocols::M4::TagsUpdateFlags']]],
-  ['dontthrowonerrorreply_65',['DontThrowOnErrorReply',['../d8/db6/namespacelogika_1_1protocols_1_1M4_1_1RecvFlags.html#ad2ac510e998b79ab611378805722573aa3bb5341d2b2108623dee5859bd678c7a',1,'logika::protocols::M4::RecvFlags']]],
+  ['dontgeteus_64',['DontGetEus',['../d5/d7f/namespacelogika_1_1protocols_1_1M4_1_1TagsUpdateFlags.html#af439796f1fab18dbdd0c8055d738f701a923f3ef96639372f096724f1835568c7',1,'logika::protocols::M4::TagsUpdateFlags']]],
+  ['dontthrowonerrorreply_65',['DontThrowOnErrorReply',['../d8/db6/namespacelogika_1_1protocols_1_1M4_1_1RecvFlags.html#a51e53bca5dbaf18164e50d4bdd29833ba3bb5341d2b2108623dee5859bd678c7a',1,'logika::protocols::M4::RecvFlags']]],
   ['double_66',['Double',['../dc/df7/namespacelogika_1_1meters.html#a3c9c221a1135a439bbbaf4a504c70fefad909d38d705ce75386dd86e611a82f5b',1,'logika::meters']]],
   ['dp_67',['dP',['../dc/df7/namespacelogika_1_1meters.html#afb48755c238c8e35d9ffd51bf0e5dd97a488ab29adec84b0394426c82ab48b26d',1,'logika::meters']]]
 ];
--- search/enumvalues_e.js
+++ search/enumvalues_e.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['r32_0',['R32',['../d6/d1f/namespacelogika_1_1meters_1_1BinaryType4L.html#ae46513347c4ea0ea5563977484872d90abd558b73141cafda516384c41b6d76ff',1,'logika::meters::BinaryType4L']]],
   ['radius_1',['Radius',['../d6/dec/namespacelogika_1_1connections_1_1ConnectionType.html#a4a8329efb416b5853518060e074f29bbae4ce8382644126fc4d6d927444e4e0d6',1,'logika::connections::ConnectionType']]],
-  ['ratedefault_2',['RateDefault',['../d7/de4/namespacelogika_1_1connections_1_1BaudRate.html#a905f10098df475c423f9fb75886c9f2baa45fc9a42c5347a2c03df023d60f2924',1,'logika::connections::BaudRate']]],
-  ['rcshift_3',['RcShift',['../d6/d61/namespacelogika_1_1Rc.html#afa2afc7dc6f2db0b311515486bfc91b0a875fac6954f820bbdca34c0ec65d15ec',1,'logika::Rc::RcShift'],['../df/dea/namespacelogika_1_1connections_1_1Rc.html#ad077f3dcd802ffc6a350f1a97877a4c6aaa224718edc5717212d8b7f1b1ac0075',1,'logika::connections::Rc::RcShift'],['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#a481bf96742509dbd4ff8fe8f255b2e1fa872492076566fbe3b59bb9488390550b',1,'logika::protocols::Rc::RcShift']]],
+  ['ratedefault_2',['RateDefault',['../d7/de4/namespacelogika_1_1connections_1_1BaudRate.html#a0665b9652add4ad2ce442920cffbc02baa45fc9a42c5347a2c03df023d60f2924',1,'logika::connections::BaudRate']]],
+  ['rcshift_3',['RcShift',['../d6/d61/namespacelogika_1_1Rc.html#afa2afc7dc6f2db0b311515486bfc91b0a875fac6954f820bbdca34c0ec65d15ec',1,'logika::Rc::RcShift'],['../df/dea/namespacelogika_1_1connections_1_1Rc.html#ad077f3dcd802ffc6a350f1a97877a4c6aaa224718edc5717212d8b7f1b1ac0075',1,'logika::connections::Rc::RcShift'],['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#ae7d7b0f88e6cebaaac662958573da295a872492076566fbe3b59bb9488390550b',1,'logika::protocols::Rc::RcShift']]],
   ['rday_4',['RDay',['../dc/df7/namespacelogika_1_1meters.html#a199715557a67e6906bb2ee196129357dadc0a71955be3dd79f1cb75c067f877ee',1,'logika::meters']]],
   ['readarchive_5',['ReadArchive',['../d8/d96/namespacelogika_1_1protocols_1_1M4_1_1Opcode.html#a399fe99af47750da7459306b1ff0736ba599ba4f04ac722af244915976a628ec4',1,'logika::protocols::M4::Opcode']]],
   ['readerror_6',['ReadError',['../df/dea/namespacelogika_1_1connections_1_1Rc.html#ad077f3dcd802ffc6a350f1a97877a4c6ad4a4478b6aa77af0150eaf497fca1b77',1,'logika::connections::Rc']]],
@@ -14,9 +14,9 @@ var searchData=
   ['reset_11',['Reset',['../da/d59/namespacelogika_1_1protocols.html#ac2249da78a8c87c4f76b71a244cc30bca526d688f37a86d3c3f27d0c5016eb71d',1,'logika::protocols']]],
   ['rhour_12',['RHour',['../dc/df7/namespacelogika_1_1meters.html#a199715557a67e6906bb2ee196129357daa7924d0a8c0bb5e5cfa106d3afa7a936',1,'logika::meters']]],
   ['rsbus_13',['RSBus',['../dc/df7/namespacelogika_1_1meters.html#aaba4278b447ba2e737ab5f95a0c14bbaaec0c8973d082934817c932a12035081d',1,'logika::meters']]],
-  ['rx_14',['Rx',['../d0/d3a/namespacelogika_1_1connections_1_1PurgeFlags.html#a112b2860a870647b1aa90be88b78c6efa46ac5284d2fd771eb4835ec2a88b0721',1,'logika::connections::PurgeFlags']]],
-  ['rxcrcerror_15',['RxCrcError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#a481bf96742509dbd4ff8fe8f255b2e1fa995fb56dec3bc0d03bd23e93189120f4',1,'logika::protocols::Rc']]],
-  ['rxgeneralerror_16',['RxGeneralError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#a481bf96742509dbd4ff8fe8f255b2e1fa90ee0fedd42e5c674b1ac926b408dacf',1,'logika::protocols::Rc']]],
-  ['rxlatepacketerror_17',['RxLatePacketError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#a481bf96742509dbd4ff8fe8f255b2e1fafc530a55244a1ea2b28fa36a3e5e1b73',1,'logika::protocols::Rc']]],
-  ['rxtimeouterror_18',['RxTimeoutError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#a481bf96742509dbd4ff8fe8f255b2e1fae71fd547de91f7f0c64ec4508fe04265',1,'logika::protocols::Rc']]]
+  ['rx_14',['Rx',['../d0/d3a/namespacelogika_1_1connections_1_1PurgeFlags.html#a296eb468d7877959170924ec598cd1aea46ac5284d2fd771eb4835ec2a88b0721',1,'logika::connections::PurgeFlags']]],
+  ['rxcrcerror_15',['RxCrcError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#ae7d7b0f88e6cebaaac662958573da295a995fb56dec3bc0d03bd23e93189120f4',1,'logika::protocols::Rc']]],
+  ['rxgeneralerror_16',['RxGeneralError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#ae7d7b0f88e6cebaaac662958573da295a90ee0fedd42e5c674b1ac926b408dacf',1,'logika::protocols::Rc']]],
+  ['rxlatepacketerror_17',['RxLatePacketError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#ae7d7b0f88e6cebaaac662958573da295afc530a55244a1ea2b28fa36a3e5e1b73',1,'logika::protocols::Rc']]],
+  ['rxtimeouterror_18',['RxTimeoutError',['../d0/d25/namespacelogika_1_1protocols_1_1Rc.html#ae7d7b0f88e6cebaaac662958573da295ae71fd547de91f7f0c64ec4508fe04265',1,'logika::protocols::Rc']]]
 ];
